b029218073d5063c258312fa9b1a550558afe60e
17804 <ameen_a@geojit.com>
2022-12-06 14:55:33 +0530

    env

--- core/db_cgo.go
+++ core/db_cgo.go
@@ -3,17 +3,18 @@ package core
 import (
 
      "fmt"
+	 "os"
 	_ "github.com/denisenkom/go-mssqldb"
 	"github.com/pocketbase/dbx"
 )
 
 func connectDB(dbPath string) (*dbx.DB, error) {
-	var server = "127.0.0.1"
-	var port = 1433
-	var user = "sa"
-	var password = "gitman"
-	var database = "pocketbase"
-	db, openErr := dbx.MustOpen("mssql",fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
+	var server = os.Getenv("DBSERVER")//192.168.29.225:1433
+	var port = os.Getenv("DBPORT")//1433
+	var user = os.Getenv("DBUSER")//"sa"
+	var password = os.Getenv("DBPASS")//"gitman"
+	var database = os.Getenv("DBNAME")//"pocketbase"
+	db, openErr := dbx.MustOpen("mssql",fmt.Sprintf("server=%s;user id=%s;password=%s;port=%s;database=%s;",
 	server, user, password, port, database))
 	if openErr != nil {
 		return nil, openErr
--- examples/base/main.go
+++ examples/base/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"net/http"
 	"regexp"
 	"strings"
@@ -17,6 +18,32 @@ import (
 )
 
 func main() {
+	_, ok := os.LookupEnv("DBSERVER")
+	if !ok {
+		log.Fatal("DBSERVER environment variable is not set")
+		return
+	}
+	_, ok = os.LookupEnv("DBPORT")
+	if !ok {
+		log.Fatal("DBPORT environment variable is not set")
+		return
+	}
+	_, ok = os.LookupEnv("DBUSER")
+	if !ok {
+		log.Fatal("DBUSER environment variable is not set")
+		return
+	}
+	_, ok = os.LookupEnv("DBPASS")
+	if !ok {
+		log.Fatal("DBPASS environment variable is not set")
+		return
+	}
+	_, ok = os.LookupEnv("DBNAME")
+	if !ok {
+		log.Fatal("DBNAME environment variable is not set")
+		return
+	}
+
 	app := pocketbase.New()
 
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
@@ -32,18 +59,28 @@ func main() {
 			Method: http.MethodPost,
 			Path:   "/api/uploadData",
 			Handler: func(c echo.Context) error {
+				// get the file and params
 				file, fileErr := c.FormFile("file")
-				tableName := c.FormValueDefault("tableName","data_csv")
+				tableName := c.FormValueDefault("tableName", "data_csv")
 				append := c.FormValueDefault("append", "false")
+
+				// check for errors in file upload
 				if fileErr == nil {
+
+					// open the file
 					fileContent, _ := file.Open()
 					reader, err := csvdict.NewReader(fileContent)
+
+					// check for errors in opening the file
 					if err != nil {
 						log.Println("1 " + err.Error())
 						return c.String(500, err.Error())
 					}
+
+					// check if the table exists
 					hasTable := app.Dao().HasTable(tableName)
-					println(append+" value")
+
+					// if the table exists and append is false, delete the table
 					if hasTable && append == "false" {
 						col, _ := app.Dao().FindCollectionByNameOrId(tableName)
 						delErr := app.Dao().DeleteCollection(col)
@@ -52,19 +89,49 @@ func main() {
 							return c.String(500, delErr.Error())
 						}
 					}
+
+					// Loop through the rows in csv file
 					count := -1
 					for {
+
+						// read the row
 						count++
 						row, err := reader.Read()
-
 						if err != nil {
 							break
 						}
+
+						// if the table doesn't exist, create it
 						hasTable = app.Dao().HasTable(tableName)
 						if !hasTable {
 							collection := &models.Collection{}
 							collection.Name = tableName
 							collection.Schema = schema.NewSchema()
+
+							// add access rules. empty string means full access
+							rule := ""
+							collection.ViewRule = &rule
+							collection.ListRule = &rule
+
+							// check csv integrity
+							if _, ok := row["Fixed asset number"]; !ok {
+								return c.String(500, "Missing \"Fixed asset number\" column")
+							}
+							if _, ok := row["Asset Name"]; !ok {
+								return c.String(500, "Missing \"Asset Name\" column")
+							}
+							if _, ok := row["Department"]; !ok {
+								return c.String(500, "Missing \"Department\" column")
+							}
+							if _, ok := row["Name"]; !ok {
+								return c.String(500, "Missing \"Department\" column")
+							}
+							if _, ok := row["Serial number"]; !ok {
+								return c.String(500, "Missing \"Serial number\" column")
+							}
+							if _, ok := row["Assigned Emp Code"]; !ok {
+								return c.String(500, "Missing \"Assigned Emp Code\" column")
+							}
 
 							for k, _ := range row {
 								key := sanitizeString(k)
@@ -73,20 +140,32 @@ func main() {
 									Type: schema.FieldTypeText,
 								})
 							}
-
+							collection.Schema.AddField(&schema.SchemaField{
+								Name: "verified",
+								Type: schema.FieldTypeBool,
+							})
 							if err := app.Dao().SaveCollection(collection); err != nil {
 								log.Println("2 " + err.Error())
 								return c.String(500, err.Error())
 							}
 
 						}
+
+						// create the document
 						collection, _ := app.Dao().FindCollectionByNameOrId(tableName)
 						r1 := models.NewRecord(collection)
+
+						// add the fields
 						for k, v := range row {
 							key := sanitizeString(k)
 							r1.SetDataValue(key, v)
 						}
+						r1.SetDataValue("verified", false)
+
+						// save the document
 						err1 := app.Dao().SaveRecord(r1)
+
+						// check for errors in saving the document
 						if err1 != nil {
 							log.Println("3 " + err1.Error())
 							return c.String(500, err1.Error())
@@ -94,7 +173,7 @@ func main() {
 
 					}
 
-					return c.String(200,fmt.Sprintf("%d records added to the collection "+tableName, count))
+					return c.String(200, fmt.Sprintf("%d records added to the collection "+tableName, count))
 				}
 				log.Println("4 " + fileErr.Error())
 				return c.String(500, fileErr.Error())
@@ -114,12 +193,17 @@ func main() {
 
 // Sanitize string
 func sanitizeString(s string) string {
+	reSp, err := regexp.Compile(`[&.]`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s = reSp.ReplaceAllString(s, "")
 	re, err := regexp.Compile(`[^\w]`)
 	if err != nil {
 		log.Fatal(err)
 	}
-	s = re.ReplaceAllString(s, "")
+	s = re.ReplaceAllString(s, "_")
 	s = strings.Replace(s, ".", "", 5)
 	s = strings.ToLower(strings.Replace(s, " ", "_", 5))
-	return s;
+	return s
 }
